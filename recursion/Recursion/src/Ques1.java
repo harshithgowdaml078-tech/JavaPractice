b8109851a4ad1c8249ffa6c24cc29b367bb5bc61
harshithgowdaml078-tech <harshithgowdaml078@gmail.com>
2026-07-27 22:13:35 +0530

    27-7-2026

---
+++ recursion/Recursion/src/Ques1.java
@@ -0,0 +1,25 @@
+public class Ques1 {
+    static void main(String[] args) {
+        print();
+    }
+
+    public static void print() {
+        System.out.println("Hello World");
+        print1();
+    }
+    public static void print1() {
+        System.out.println("Hello World");
+        print2();
+    }
+    public static void print2() {
+        System.out.println("Hello World");
+        print3();
+    }
+    public static void print3() {
+        System.out.println("Hello World");
+        print4();
+    }
+    public static void print4() {
+        System.out.println("Hello World");
+    }
+}
